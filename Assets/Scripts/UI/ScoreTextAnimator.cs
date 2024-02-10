2d9c7225daf2493ad344a187ab7bf8bbc45e4393
rumkkee <accesareo@gmail.com>
2024-02-09 22:23:20 -0800

    Added anticipation to the font size change

--- Assets/Scripts/UI/ScoreTextAnimator.cs
+++ Assets/Scripts/UI/ScoreTextAnimator.cs
@@ -21,10 +21,33 @@ public class ScoreTextAnimator : MonoBehaviour
     public IEnumerator GrowText(TextMeshProUGUI text)
     {
         float timePassed = 0;
+
+        float shrunkTextSize = normalTextSize * 0.4f;
         do
         {
             timePassed += Time.deltaTime;
-            text.fontSize = Mathf.Lerp(largeTextSize, normalTextSize, timePassed * speedMultiplier);
+            text.fontSize = Mathf.SmoothStep(normalTextSize, shrunkTextSize, timePassed * speedMultiplier);
+            yield return null;
+        }
+        while(text.fontSize != shrunkTextSize);
+
+        timePassed = 0;
+
+        do
+        {
+            timePassed += Time.deltaTime;
+            text.fontSize = Mathf.SmoothStep(shrunkTextSize, largeTextSize, timePassed * speedMultiplier * 2);
+            yield return null;
+        }
+        while (text.fontSize != largeTextSize);
+
+        timePassed = 0;
+
+        yield return null;
+        do
+        {
+            timePassed += Time.deltaTime;
+            text.fontSize = Mathf.SmoothStep(largeTextSize, normalTextSize, timePassed * speedMultiplier * 1.5f);
             yield return null;
         }
         while (text.fontSize != normalTextSize);        
